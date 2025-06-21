09f784fa3dbe5b6993a7b79becf007b4f58879c6
unknown <philcinamula@gmail.com>
2025-06-21 23:18:50 +0300

    Implemented Auth, Billing, Database Setup, Create Companion Server action

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,52 @@
 import { Button } from "@/components/ui/button";
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/CTA";
+import { recentSessions } from "@/constants";
 
 const Page = () => {
   return (
-    <div>
-      <h1 className='text-2xl underline'>welcome to this app</h1>
-      <Button>
-        let's get started
-      </Button>
-    </div>
+    <main>
+      <h1 className='text-2xl underline'>Popular Companions</h1>
+      <section className="home-section">
+        <CompanionCard
+        id="123"
+        name="Neural the brainy explorer"
+        topic="Neural Network of thhr Brain"
+        subject="science"
+        duration={45}
+        color="#ffda6e" 
+        />
+        <CompanionCard
+        id="1235"
+        name="Countsy the Number Wizard"
+        topic="Derivatives & Integrals"
+        subject="maths"
+        duration={50} 
+        color="#e5d0ff" 
+        />
+        <CompanionCard
+        id="1234"
+        name="climate"
+        topic="weather"
+        subject="geography"
+        duration={45}
+        color="#add8e6" 
+        />
+        
+
+      </section>
+
+      <section className="home-section">
+        <CompanionsList
+        title="Recently completed sessions"
+        companions={recentSessions}
+        classNames="w-2/3 max-lg:w-full"
+        />
+        <CTA/>
+
+      </section>
+    </main>
   )
 }
 
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import NavItems from "./NavItems";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 const Navbar = () => {
   return (
@@ -13,8 +14,14 @@ const Navbar = () => {
         </Link>
         <div className="flex items-center gap-8">
             <NavItems />
-            <p>Sign in</p>
-
+            <SignedOut>
+                <SignInButton>
+                  <button className="btn-signin">Sign in</button>
+                </SignInButton>
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
         </div>
     </nav>
   )
